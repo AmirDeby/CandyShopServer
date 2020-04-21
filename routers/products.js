1832b468db1d385acea00376f0392ec8481d8c7c
AmirDeby <mizi2@bezeqint.net>
2020-04-21 17:42:17 +0300

    add product end point

--- routers/products.js
+++ routers/products.js
@@ -4,8 +4,10 @@ const { getProducts } = require('../queries')
 router.get('/', async (req, res) => {
 
     const [result] = await getProducts();
+    setTimeout(() => {
+        res.status(200).send(result);
+    }, 2000)
 
-    res.send(result);
 });
 
 
